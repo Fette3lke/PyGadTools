3640c8b77d257512a243b2df9dc4db5d71fc1086
Ludwig Oser <ludwigoser@gmail.com>
2014-11-20 17:05:30 +0100

    bugfix in phaseDiagram

--- astro/plotting/phaseDiagram.py
+++ astro/plotting/phaseDiagram.py
@@ -9,7 +9,7 @@ def phaseDiagram(self, *args, **kwargs):
 	else:
 		symbol = args[0]
 
-	plt.plot(np.log(self.rho / (astro.protonmass / astro.hydrogen_fraction / self.unit_density_in_cgs)), np.log(self.temp), 
+	plt.plot(np.log10(self.rho / (astro.protonmass / astro.hydrogen_fraction / self.unit_density_in_cgs)), np.log10(self.temp), 
 		symbol, alpha=alpha , **kwargs)
 	plt.xlabel(r'$log(\rho) \; [cm^{-3}]$')
 	plt.ylabel(r'$log(T) \; [K]$')
